bda486660aab9c19cc36111c699da9683b6e9692
Luca Giuliani <giulianil@microsoft.com>
2026-03-28 20:36:47 +0100

    Fixed file existance check in benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -7,7 +7,7 @@ dd if=/dev/urandom of=/tmp/big.bin bs=1M count=$FILESIZE_MB
 
 # Measure the time taken by each SHA-1 implementation
 for i in basic asm-x64 intrinsics-sse dotnet_wrapper; do
-    if [ -n ./sha1$i.so ] ; then
+    if [ -f ./sha1$i.so ] ; then
         time ./my_sha1 ./sha1$i.so /tmp/big.bin
     else
         echo "-> Variant $i not available" 1>&2
